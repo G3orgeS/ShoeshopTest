a47f1e0b3d2a3563f1a5b11320722851266ca878
G3orgeS <112631563+G3orgeS@users.noreply.github.com>
2023-08-24 14:09:40 +0200

    test med ändring av kvantitet

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 "use strict";
-// console.log('hej')
+// console.log('hello world')
 function createProduct(name, price, quantity) {
     return {
         name,
@@ -7,36 +7,46 @@ function createProduct(name, price, quantity) {
         quantity
     };
 }
+;
 function calculateTotalCost(products) {
     const totalCost = products.reduce((total, product) => total + product.price * product.quantity, 0);
     return totalCost;
 }
+;
 function updateQuantity(product, newQuantity) {
     product.quantity = newQuantity;
 }
+;
 document.addEventListener('DOMContentLoaded', () => {
     var _a;
     const productNameInput = document.getElementById('productName');
     const productPriceInput = document.getElementById('productPrice');
     const productQuantityInput = document.getElementById('productQuantity');
     const productList = document.getElementById('productList');
-    const totalCostElement = document.getElementById('totalCost');
     const products = [];
     function renderProducts() {
         productList.innerHTML = '';
         products.forEach(product => {
             const li = document.createElement('li');
-            li.textContent = `${product.name} - Pris: ${product.price} kr - Kvantitet: ${product.quantity}`;
+            const totalCost = calculateTotalCost(products);
+            li.textContent = `${product.name} - Pris: ${product.price} kr - Kvantitet: ${product.quantity} - Totalsumman: ${totalCost} kr`;
             productList.appendChild(li);
         });
     }
+    ;
     (_a = document.getElementById('addProduct')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', () => {
         const productName = productNameInput.value;
         const productPrice = parseFloat(productPriceInput.value);
         const productQuantity = parseFloat(productQuantityInput.value);
         if (productName && !isNaN(productPrice) && !isNaN(productQuantity)) {
-            const newProduct = createProduct(productName, productPrice, productQuantity);
-            products.push(newProduct);
+            const existingProductIndex = products.findIndex(product => product.name === productName);
+            if (existingProductIndex !== -1) {
+                products[existingProductIndex].quantity += productQuantity;
+            }
+            else {
+                const newProduct = createProduct(productName, productPrice, productQuantity);
+                products.push(newProduct);
+            }
             renderProducts();
             productNameInput.value = '';
             productPriceInput.value = '';
--- index.ts
+++ index.ts
@@ -1,33 +1,30 @@
-// console.log('hej')
+// console.log('hello world')
 
 interface Product {
     name: string;
     price: number;
     quantity: number;
 }
-
 function createProduct(name: string, price: number, quantity: number): Product {
     return {
         name,
         price,
         quantity
-    }
-}
-
+    };
+};
 function calculateTotalCost(products: Product[]): number {
     const totalCost = products.reduce((total, product) => total + product.price * product.quantity, 0)
     return totalCost
-}
+};
 
 function updateQuantity(product: Product, newQuantity: number): void {
     product.quantity = newQuantity
-}
+};
 document.addEventListener('DOMContentLoaded', () => {
     const productNameInput = document.getElementById('productName') as HTMLInputElement;
     const productPriceInput = document.getElementById('productPrice') as HTMLInputElement;
     const productQuantityInput = document.getElementById('productQuantity') as HTMLInputElement;
     const productList = document.getElementById('productList')!;
-    const totalCostElement = document.getElementById('totalCost');
 
     const products: Product[] = [];
 
@@ -35,10 +32,11 @@ document.addEventListener('DOMContentLoaded', () => {
         productList.innerHTML = '';
         products.forEach(product => {
             const li = document.createElement('li');
-            li.textContent = `${product.name} - Pris: ${product.price} kr - Kvantitet: ${product.quantity}`;
+            const totalCost = calculateTotalCost(products);
+            li.textContent = `${product.name} - Pris: ${product.price} kr - Kvantitet: ${product.quantity} - Totalsumman: ${totalCost} kr`;
             productList.appendChild(li);
         });
-    }
+    };
 
     document.getElementById('addProduct')?.addEventListener('click', () => {
         const productName = productNameInput.value;
@@ -46,15 +44,20 @@ document.addEventListener('DOMContentLoaded', () => {
         const productQuantity = parseFloat(productQuantityInput.value);
 
         if (productName && !isNaN(productPrice) && !isNaN(productQuantity)) {
-            const newProduct = createProduct(productName, productPrice, productQuantity);
-            products.push(newProduct);
 
+            const existingProductIndex = products.findIndex(product => product.name === productName);
+
+            if (existingProductIndex !== -1) {
+                products[existingProductIndex].quantity += productQuantity
+            } else {
+                const newProduct = createProduct(productName, productPrice, productQuantity);
+                products.push(newProduct);
+            }
             renderProducts();
 
             productNameInput.value = '';
             productPriceInput.value = '';
             productQuantityInput.value = '';
         }
-
     });
 });
